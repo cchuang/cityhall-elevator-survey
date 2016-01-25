22d49eb26dd6feed039f06233995e48216816e74
Cheng-Ching Huang <cchuang.tw@gmail.com>
2016-01-25 17:14:46 +0800

    analyze.R: remove moved functions

--- analyze.R
+++ analyze.R
@@ -1,7 +1,7 @@
 #!/bin/
 # author: cchuang
 # description: analyze the efficiency of cityhall's elevators
-# setwd("D:/tpc.elev");source("analyze.R")
+# setwd("D:/cityhall-elevator-survey");source("analyze.R")
 #library(doParallel)
 library(plyr)
 library(foreach)
@@ -18,100 +18,126 @@ cpt.data <- data.frame()
 ########################################
 # Functions
 
-# default started at 2015-12-30 11:00:00
-RmTvEntries <- function(x, start_time=1451444400) {
-	# sorting
-	x <- x[ with(x, order(Time)), ]
-	rl.res <- rle(x$Val)
-	cumsum(rl.res$lengths)
-	# discard the first and the last sample due to incomplete record period. 
-	edge <- c(cumsum(rl.res$lengths)[-length(rl.res$lengths)] + 1) 
-	x.tmod <- x[edge, ]
-	x.tmod$Tick <- x.tmod$Time
-	x.tmod$Time <- as.POSIXct(x.tmod$Tick/1000 + start_time, origin="1970-01-01")
-
-	# load/store global variable
-	cpt.data <- get("cpt.data", envir = .GlobalEnv) 
-	cpt.data <- rbind(cpt.data, x.tmod) 
-	assign("cpt.data", cpt.data, envir = .GlobalEnv) 
-
-	return(nrow(x))
-}
-
-CountTimeDuration <- function(x, out.data=data.frame()) {
-	# sorting
-	x <- x[ with(x, order(Time)), ]
-	if (x$Val[1] != 1) {
-		x <- x[-1,]
-	}
-	if (nrow(x) %% 2 == 1) {
-		x <- x[-nrow(x),]
-	}
-	if (nrow(x) == 0) {
-		return(0)
-	}
-	
-	# Even rows of x
-	x.even <- x[seq(2, nrow(x), 2),]
-	# Odd rows of x
-	x.odd <- x[seq(1, nrow(x), 2),]
-	browser("The events are NOT arranged as 1 0 1 0...", expr=(x.even$Val!=0 || x.odd$Val!=1))
-
-	x.odd$Duration <- x.even$Time - x.odd$Time
-	x.odd$Val <- NULL
-	eval(substitute(out.data<-rbind(out.data, data.frame(x.odd))), envir=.GlobalEnv)
-	return(nrow(x.odd))
-}
-
-CountMultiSummoning <- function(x, full.data, event) {
-	y <- list(cnt=nrow(x), unchanged=0, multi.waiting=0, uc.rate=0)
-	curr.grp <- unique(x$CarGrp)
-	cooldown <- vector()
-	browser("Must have only one group", expr=(length(curr.grp)!=1))
-
-	full.grps <- unique(full.data$CarGrp)
-	other.grps <- full.grps[full.grps != curr.grp]
-	y[full.grps] <- 0
-	for (i in 1:nrow(x)) {
-		xrow <- x[i,]
-		# drop those samples that are 2nd press
-		# 	we don't drop this because the number looks too small 
-		# 	and there are more circumstances that we can still count it
-		#focus.rows <- subset(full.data, Event == event 
-		#	& CarGrp != xrow$CarGrp
-		#	& Fl == xrow$Fl
-		#	& Time < xrow$Time)
-		#focus.rows <- focus.rows[(xrow$Time - focus.rows$Time) < focus.rows$Duration, ]
-		#if (nrow(focus.rows) != 0) {
-		#	next
-		#}
-
-		focus.rows <- subset(full.data, Event == event 
-			& CarGrp != xrow$CarGrp
-			& Fl == xrow$Fl
-			& Time > xrow$Time)
-		focus.rows <- focus.rows[(focus.rows$Time - xrow$Time) < xrow$Duration, ]
-		if (nrow(focus.rows) != 0) {
-			cooldown[length(cooldown) + 1] <- min(focus.rows$Time - xrow$Time)
-
-			focus.grps <- unique(focus.rows$CarGrp)
-			for (grp in focus.grps) {
-				y[[grp]] <- y[[grp]] + 1 
-			}
-			if (all((focus.rows$Time + focus.rows$Duration) >= (xrow$Time + xrow$Duration))) {
-				y$unchanged <- y$unchanged + 1
-			}
-		}
-
-	}
-	y$multi.waiting <- length(cooldown)
-	y$uc.rate <- y$unchanged / y$multi.waiting
-	y$cd.median <- median(cooldown)
-	y$cd.mean <- mean(cooldown)
-	return(data.frame(y))
-}
-#ddply(subset(hf.data, Event=="REQDOWN"), .(CarGrp),  CountMultiSummoning, full.data=hf.data, event="REQDOWN")
-
+## default started at 2015-12-30 11:00:00
+#RmTvEntries <- function(x, start_time=1451444400) {
+#	# sorting
+#	x <- x[ with(x, order(Time)), ]
+#	rl.res <- rle(x$Val)
+#	cumsum(rl.res$lengths)
+#	# discard the first and the last sample due to incomplete record period. 
+#	edge <- c(cumsum(rl.res$lengths)[-length(rl.res$lengths)] + 1) 
+#	x.tmod <- x[edge, ]
+#	x.tmod$Tick <- x.tmod$Time
+#	x.tmod$Time <- as.POSIXct(x.tmod$Tick/1000 + start_time, origin="1970-01-01")
+#
+#	# load/store global variable
+#	cpt.data <- get("cpt.data", envir = .GlobalEnv) 
+#	cpt.data <- rbind(cpt.data, x.tmod) 
+#	assign("cpt.data", cpt.data, envir = .GlobalEnv) 
+#
+#	return(nrow(x))
+#}
+#
+## one x event endded when y event were happening
+#HasIntersection <- function(x, y) {
+#	end.time <- x$Time + x$Duration
+#	return(any(end.time >= y$Time & end.time <= (y$Time + y$Duration)))
+#}
+#
+#MergeCarChEvents <- function(x) {
+#	x <- x[ with(x, order(Time)), ]
+#	# input must have time duration info
+#	for (i in 1:nrow(x)) {
+#		r <- x[i,]
+#		if (!((r$Event == "REQUP") | (r$Event == "REQDOWN"))) {
+#			next
+#		} else {
+#			print(r)
+#		}
+#		if (!HasIntersection(r, 
+#			subset(x, Event=="CARISHERE" &
+#				   CarGrp==r$CarGrp & 
+#				   Fl==r$Fl))) {
+#			#print(r)
+#		}
+#	}
+#	return(x)
+#}
+#
+#CountTimeDuration <- function(x, out.data=data.frame()) {
+#	# sorting
+#	x <- x[ with(x, order(Time)), ]
+#	if (x$Val[1] != 1) {
+#		x <- x[-1,]
+#	}
+#	if (nrow(x) %% 2 == 1) {
+#		x <- x[-nrow(x),]
+#	}
+#	if (nrow(x) == 0) {
+#		return(0)
+#	}
+#	
+#	# Even rows of x
+#	x.even <- x[seq(2, nrow(x), 2),]
+#	# Odd rows of x
+#	x.odd <- x[seq(1, nrow(x), 2),]
+#	browser("The events are NOT arranged as 1 0 1 0...", expr=(x.even$Val!=0 || x.odd$Val!=1))
+#
+#	x.odd$Duration <- x.even$Time - x.odd$Time
+#	x.odd$Val <- NULL
+#	eval(substitute(out.data<-rbind(out.data, data.frame(x.odd))), envir=.GlobalEnv)
+#	return(nrow(x.odd))
+#}
+#
+#CountMultiSummoning <- function(x, full.data, event) {
+#	y <- list(cnt=nrow(x), unchanged=0, multi.waiting=0, uc.rate=0)
+#	curr.grp <- unique(x$CarGrp)
+#	cooldown <- vector()
+#	browser("Must have only one group", expr=(length(curr.grp)!=1))
+#
+#	full.grps <- unique(full.data$CarGrp)
+#	other.grps <- full.grps[full.grps != curr.grp]
+#	y[full.grps] <- 0
+#	for (i in 1:nrow(x)) {
+#		xrow <- x[i,]
+#		# drop those samples that are 2nd press
+#		# 	we don't drop this because the number looks too small 
+#		# 	and there are more circumstances that we can still count it
+#		#focus.rows <- subset(full.data, Event == event 
+#		#	& CarGrp != xrow$CarGrp
+#		#	& Fl == xrow$Fl
+#		#	& Time < xrow$Time)
+#		#focus.rows <- focus.rows[(xrow$Time - focus.rows$Time) < focus.rows$Duration, ]
+#		#if (nrow(focus.rows) != 0) {
+#		#	next
+#		#}
+#
+#		focus.rows <- subset(full.data, Event == event 
+#			& CarGrp != xrow$CarGrp
+#			& Fl == xrow$Fl
+#			& Time > xrow$Time)
+#		focus.rows <- focus.rows[(focus.rows$Time - xrow$Time) < xrow$Duration, ]
+#		if (nrow(focus.rows) != 0) {
+#			cooldown[length(cooldown) + 1] <- min(focus.rows$Time - xrow$Time)
+#
+#			focus.grps <- unique(focus.rows$CarGrp)
+#			for (grp in focus.grps) {
+#				y[[grp]] <- y[[grp]] + 1 
+#			}
+#			if (all((focus.rows$Time + focus.rows$Duration) >= (xrow$Time + xrow$Duration))) {
+#				y$unchanged <- y$unchanged + 1
+#			}
+#		}
+#
+#	}
+#	y$multi.waiting <- length(cooldown)
+#	y$uc.rate <- y$unchanged / y$multi.waiting
+#	y$cd.median <- median(cooldown)
+#	y$cd.mean <- mean(cooldown)
+#	return(data.frame(y))
+#}
+##ddply(subset(hf.data, Event=="REQDOWN"), .(CarGrp),  CountMultiSummoning, full.data=hf.data, event="REQDOWN")
+#
 target.time1 <- 1451444819
 target.time2 <- 0
 target.time <- as.POSIXct(target.time1 + target.time2, origin="1970-01-01")
@@ -134,10 +160,9 @@ nrow.data <- ddply(orig.data, .(Car, Fl, Event), RmTvEntries, start_time=target.
 ###########################################################
 # Waiting Time
 wt.data <- data.frame()
-ddply(subset(cpt.data, Event=="REQUP" | Event=="REQDOWN" | Event=="REQSTOP" | Event=="DOORISOPEN"), 
-	  .(Car, Fl, Event), CountTimeDuration, out.data=wt.data)
+ddply(cpt.data, .(Car, Fl, Event), CountTimeDuration, out.data=wt.data)
 
-wt.data2 <- within(wt.data, { 
+wt.data <- within(wt.data, { 
 	Duration <- as.numeric(Duration)
 	Fl <- as.factor(Fl)
 	CarGrp <- character(length = length(Car))
@@ -145,6 +170,11 @@ wt.data2 <- within(wt.data, {
 	CarGrp[Car == "NC4" | Car == "NC5"] <- "CG2"
 	CarGrp[Car == "NC6"] <- "CG3"
 })
+
+MergeCarChEvents(wt.data) 
+stop("Stop for debugging")
+
+wt.data2 <- subset(wt.data, Event=="REQUP" | Event=="REQDOWN" | Event=="REQSTOP" | Event=="DOORISOPEN")
 
 #p <- ggplot(wt.data2, aes(Fl, Duration)) 
 #print(p + geom_boxplot() + facet_grid(CarGrp ~ Event))
